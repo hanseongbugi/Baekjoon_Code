7357d82f1a9cd335aa0e858b168ab562d7e90455
hanseongbugi <105718365+hanseongbugi@users.noreply.github.com>
2023-09-30 19:37:33 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    string str;
+    cin>>str;
+    int i;
+    cin>>i;
+    cout<<str[i-1]<<'\n';
+}
