e7c2f89d3d299b1c8dd8750ffdd49b20926abfae
hanseongbugi <105718365+hanseongbugi@users.noreply.github.com>
2023-09-30 21:25:10 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int sum = 0;
+    for(int i =0;i<5;i++){
+        int n;
+        cin>>n;
+        sum+=n*n;
+    }
+    cout<<sum%10<<'\n';
+}
