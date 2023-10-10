6f8131840c30c8dad9b1e0ea5ec747b30bb79945
hanseongbugi <105718365+hanseongbugi@users.noreply.github.com>
2023-10-10 19:33:41 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 276 ms, Memory: 8292 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -2,7 +2,7 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
-
+ 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -16,9 +16,7 @@ int main(){
         arr.push_back(input);
     }
     sort(arr.begin(),arr.end());
-    for(int i = 0;i<arr.size() - 1;i++){
-        if(arr[i] == arr[i+1]) continue;
+    for(int i = 0;i<arr.size();i++){
         cout<<arr[i]<<'\n';
     }
-    cout<<arr[arr.size()-1]<<'\n';
 }
