74e29ec22f2806646e28c15484fe60930dc2bc53
hanseongbugi <105718365+hanseongbugi@users.noreply.github.com>
2023-10-11 16:54:47 +0900

    Update 통계학.cc

--- 백준/Silver/2108. 통계학/통계학.cc
+++ 백준/Silver/2108. 통계학/통계학.cc
@@ -1,44 +1,51 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <cmath>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cmath>
 using namespace std;
-vector<int> arr;
-int main() {
-    int num,tmp,range,middle = 0,most_val,mean = 0;
-    int most = -9999;
-    int number[8001] = {0,};
-    bool not_first = false;
-    cin >> num;
-    for(int i = 0; i < num; i++)
-    {
-        cin >> tmp;
-        arr.push_back(tmp);
-        mean += tmp;
-        number[tmp+4000]++;
+ 
+int counter[8001] = {0,};
+ 
+int main(){
+    int N;
+    cin>>N;
+    vector<int> arr;
+    arr.assign(N,0);
+    double avg = 0;
+    for(int i = 0;i<N;i++){
+        int input;
+        cin>>input;
+        arr[i] = input;
+        avg += input;
+        counter[input+4000] ++;
     }
+    avg /= N; // 산술 평균 연산
     sort(arr.begin(),arr.end());
-    for(int i = 0; i < 8001; i++)
-    {
-        if(number[i] == 0)
-            continue;
-        if(number[i] == most)
-        {
-            if(not_first)
-            {
-                most_val = i - 4000;
+    int mid = arr[N/2]; // 중앙 값 연산
+ 
+    int min = 0;
+    int most = -9999;
+    bool not_first = false;
+    for(int i = 0;i<8001; i++){
+        if(counter[i] == 0) continue;
+        if(counter[i] == most){
+            if(not_first){
+                min = i - 4000;
                 not_first = false;
             }
         }
-        if(number[i] > most)
-        {
-            most = number[i];
-            most_val = i - 4000;
+        if(counter[i] > most){
+            most = counter[i];
+            min = i - 4000;
             not_first = true;
         }
     }
-    middle = arr[arr.size()/2];
-    mean = round((float)mean / num);
-    range = arr.back() - arr.front();
-    cout << mean << '\n' << middle << '\n' << most_val << '\n' << range;
+  
+    int range = arr[N-1] - arr[0]; // 범위 연산
+    double average = round(avg);
+    if(avg < 0.5 && avg>-0.5) average = 0;
+    cout<<average<<'\n';
+    cout<<mid<<'\n';
+    cout<<min<<'\n';
+    cout<<range<<'\n';
 }
