71f067da127c3176c9c22ce07064fac52778f788
hanseongbugi <105718365+hanseongbugi@users.noreply.github.com>
2023-11-10 16:19:44 +0900

    [Silver V] Title: BABBA, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/9625. BABBA/BABBA.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int K;
+int dp[46][46];
+
+int main() {
+	cin >> K;
+	dp[0][0] = 1;
+	dp[0][1] = 0;
+	dp[1][0] = 0;
+	dp[1][1] = 1;
+	for (int i = 2; i <= K; i++) {
+		dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+		dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+	}
+
+	cout << dp[K][0] << " " << dp[K][1] << '\n';
+
+}
