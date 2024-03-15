bdf04eceaffaec04dc23a4daf1eb116ccf74afb3
hanseongbugi <105718365+hanseongbugi@users.noreply.github.com>
2024-03-15 16:05:36 +0900

    [Gold II] Title: 꿈틀꿈틀 호석 애벌레 - 효율성, Time: 12 ms, Memory: 3192 KB, Score: 66 point -BaekjoonHub

--- 백준/Gold/20181. 꿈틀꿈틀 호석 애벌레 － 효율성/꿈틀꿈틀 호석 애벌레 － 효율성.cc
+++ 백준/Gold/20181. 꿈틀꿈틀 호석 애벌레 － 효율성/꿈틀꿈틀 호석 애벌레 － 효율성.cc
@@ -1,44 +1,32 @@
 #include<iostream>
 using namespace std;
+ 
+int N, K;
+long long sum, dp[100001], temp, ans;
+int arr[100001];
+ 
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+ 
+	cin >> N >> K;
 
-int N,K;
-long long arr[100001];
-long long dp[100001];
+	for (int i = 0;i<N;i++) 
+        cin >> arr[i];
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    
-    cin>>N>>K;
+    int start = 0, end = 0;
 
-    for(int i = 0;i<N;i++){
-        cin>>arr[i];
-    }
-    
-    int start = 1, end = 1;
-    long long temp = arr[start];
+	while (end <= N) {
+		if (sum >= K) {
+			temp = (start == 0 ? 0 : max(temp, dp[start - 1]));
+			dp[end - 1] = max(dp[end - 1], temp + sum - K);
+			sum -= arr[start++];
+		}
+		else sum += arr[end++];
+	}
 
-    while(start<=N&&end<=N){
-        dp[end] = max(dp[end],dp[end-1]);
+	for (int i = 0; i < N; i++)
+		ans = max(ans, dp[i]);
 
-        if(temp>=K){
-            dp[end] = max(dp[end],dp[start-1] + temp - K);
-            if(start < end){
-                temp -= arr[start];
-                start++;
-            }
-            else if(start == end){
-                start++;
-                end++;
-                temp = arr[end];
-            }
-        }
-        else{
-            end++;
-            temp+=arr[end];
-        }
-    }
-    cout<<dp[N]<<'\n';
-
+	cout << ans<<'\n';
 }
