96b52157cad3759a64455cbf46825e4900f6b8b6
hanseongbugi <105718365+hanseongbugi@users.noreply.github.com>
2024-06-01 15:23:19 +0900

    Update 모의고사.cpp

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -7,7 +7,7 @@ int op1[] = {1, 2, 3, 4, 5};
 int op2[] = {2, 1, 2, 3, 2, 4, 2, 5};
 int op3[] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 int N;
-int st1, st2, st3;
+int st[3];
 
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
@@ -15,47 +15,17 @@ vector<int> solution(vector<int> answers) {
     for(int i = 0;i<N;i++){
         int num = answers[i];
         if(op1[i%5] == num)
-            st1 += 1;
+            st[0] += 1;
         if(op2[i%8] == num)
-            st2 += 1;
+            st[1] += 1;
         if(op3[i%10] == num)
-            st3 += 1;
+            st[2] += 1;
     }
-    if(st1 == st2){
-        if(st1 == st3){
-            answer.push_back(1);
-            answer.push_back(2);
-            answer.push_back(3);
-        }
-        else if(st1 < st3){
-            answer.push_back(3);
-        }
-        else{
-            answer.push_back(1);
-            answer.push_back(2);
-        }
-    }
-    else if(st1 > st2){
-        if(st3 > st1)
-            answer.push_back(3);
-        else if(st1 > st3)
-            answer.push_back(1);
-        else{
-            answer.push_back(1);
-            answer.push_back(3);
-        }
-    }
-    else{
-        if(st2 > st3){
-            answer.push_back(2);
-        }
-        else if(st3 > st2){
-            answer.push_back(3);
-        }
-        else{
-            answer.push_back(2);
-            answer.push_back(3);
-        }
+    int maxNum = max(max(st[0], st[1]), st[2]);
+    
+    for(int i = 0;i<3;i++){
+        if(maxNum == st[i]) 
+            answer.push_back(i+1);
     }
     
     return answer;
