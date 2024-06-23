758b9b47c8823bfa7a46acea4274a984f179dfa5
hanseongbugi <105718365+hanseongbugi@users.noreply.github.com>
2024-06-23 15:27:43 +0900

    [level 2] Title: 숫자의 표현, Time: 0.04 ms, Memory: 3.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+
+    for(int i = 1;i <= n;i++){
+        int sum = 0;
+        for(int j = i;j <= n;j++){
+            sum += j;
+            if(sum > n) break;
+            if(sum == n)
+                answer++;
+        }
+    }
+    
+    return answer;
+}
