dadd67fa3a73b03a949e0b26ce2fb0fe6dd99e6f
hanseongbugi <105718365+hanseongbugi@users.noreply.github.com>
2024-08-30 15:08:22 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 40 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.cc
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.cc
@@ -1,40 +1,34 @@
 #include <iostream>
+#include <cstring>
 #include <queue>
 using namespace std;
 
-queue<pair<int,int> > q;
-int graph[100][100] = {0, };
-int visited[100] = {0, };
-int N, M, counter = 5001, person;
+int N, M;
+int arr[101][101];
+bool visited[101];
+int answer = 0;
+int cavin = 987654321;
 
-void init(){
-	for(int i = 1; i<=N;i++){
-		visited[i] = 0;
-	}
-}
+int dfs(int target, int goal){
+    queue<pair<int,int>> q;
+    q.push({target,0});
+    visited[target] = true;
 
-void bfs(int start){
-	int kabin = 0;
-	q.push(pair<int, int>(start, 0));
-	visited[start] = 1;
-
-	while(!q.empty()){
-		int now = q.front().first;
-		int num = q.front().second;
-		num += 1;
-		q.pop();
-		for(int next = 1; next <= N; next++){
-			if(!visited[next] && graph[now][next]){
-				q.push(pair<int,int>(next,num));
-				visited[next] = 1;
-				kabin += num;
-			}
-		}
-	}
-	if(counter>kabin){
-		person = start;
-		counter = kabin;
-	}
+    while(!q.empty()){
+        int go = q.front().first;
+        int num = q.front().second;
+        q.pop();
+        if(go == goal){
+            return num;
+        }
+        for(int i = 1;i<=N;i++){
+            if(arr[go][i] && !visited[i]){
+                q.push({i,num + 1});
+                visited[i] = true;
+            }
+        }
+    }
+    return 0;
 }
 
 int main(){
@@ -42,16 +36,33 @@ int main(){
     cin.tie(NULL);
     cout.tie(NULL);
     
-	cin>>N>>M;
-	
-	for(int i = 0;i<M;i++){
-		int u, f; 
-		cin>>u>>f;
-		graph[u][f] = 1; graph[f][u] = 1;
-	}
-	for(int i = 1; i<=N; i++){
-		init();
-		bfs(i);
-	}
-	cout<<person<<'\n';
+    // 1 - 3    1 - 3 - 2 
+    // 1 - 4      - 4 - 5
+    // 2 - 3
+    // 3 - 4
+    // 4 - 5
+    cin>>N>>M;
+    int go, to;
+    for(int i = 0;i<M;i++){
+        cin>>go>>to;
+        arr[go][to] = 1;
+        arr[to][go] = 1;
+    }
+
+    for(int i = 1;i<=N;i++){
+        int temp = 0;
+        for(int j = 1;j<=N;j++){
+            if(j==i)
+                continue;
+            
+            memset(visited,false,sizeof(visited));
+            temp += dfs(i,j);
+        }
+        if(cavin > temp){
+            cavin = temp;
+            answer = i;
+        }
+    }
+
+    cout<<answer<<'\n';
 }
