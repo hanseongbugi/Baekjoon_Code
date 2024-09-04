bd778a8bbe7b4b4df476390503730d4c742e7795
hanseongbugi <105718365+hanseongbugi@users.noreply.github.com>
2024-09-04 17:02:13 +0900

    [Silver III] Title: N과 M (7), Time: 412 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int number[9];
+int arr[9];
+bool visited[9];
+
+void dfs(int cnt){
+    if(cnt == M){
+        for(int i = 0;i<M;i++)
+            cout<<arr[i]<<' ';
+        cout<<'\n';
+        return;
+    }
+    for(int i = 0;i<N;i++){
+        int n = number[i];
+        arr[cnt] = n;
+        dfs(cnt + 1);
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin>>N>>M;
+    for(int i = 0;i<N;i++)
+        cin>>number[i];
+    sort(number, number + N);
+
+    dfs(0);
+}   
