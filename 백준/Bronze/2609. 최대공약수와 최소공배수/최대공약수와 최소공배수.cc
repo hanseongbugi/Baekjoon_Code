6dce1530ab4e5aa84097ab6dc3e163d9f5640444
hanseongbugi <105718365+hanseongbugi@users.noreply.github.com>
2024-10-20 14:23:08 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -1,21 +1,28 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
-    int n1,n2;
-    cin>>n1>>n2;
-    unsigned long max, min;
-    int t = 0;
-    if(n1>n2) t = n2;
-    else t = n1;
-    for(int i =t;i>0;i--){
-        if(n1%i==0&&n2%i==0){
-            max = i;  
-            break;
-        } 
-    }
-    min = (n1/max) * (n2/max) * max;
+int n1, n2;
+int ans1 = 0, ans2 = 0;
+vector<int> arr;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
     
-    cout<<max<<'\n';
-    cout<<min<<'\n';
+    cin>>n1>>n2;
+
+    for(int i = 1;i<=n1;i++){
+        if(n1 % i == 0)
+            arr.push_back(i);
+    }
+    for(int i = 0;i<arr.size();i++){
+        if(n2 % arr[i] == 0)
+            ans1 = max(ans1, arr[i]);
+    }
+    ans2 = ans1 * (n1 / ans1) * (n2 / ans1);
+
+    cout<<ans1<<'\n';
+    cout<<ans2<<'\n';
 }
